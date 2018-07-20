ec7160e2587058623ad0b0a64bd38c08add6f5d3
Frederick Ofori <FrederickCodes@gmail.com>
2018-07-20 10:07:58 +0100

    added more weather elements, wrapped in function. To be refactored

--- scripts/freddie.js
+++ scripts/freddie.js
@@ -1,43 +1,63 @@
 window.onload = function(){
-    const apiKey = "814e605f58e6aaf7e798e6161f0e73fb";
-
-    function createELement(tag, parent, text = "" ,attributeTag, attributeInfo){
-        let element = document.createElement(tag);
-        let node = document.createTextNode(text);
-        if (attributeTag) {
-            element.setAttribute(attributeTag, attributeInfo)
+    function createWeatherBlock(){
+        const apiKey = "814e605f58e6aaf7e798e6161f0e73fb";
+    
+        function createELement(tag, parent, text = "" ,attributeTag, attributeInfo){
+            let element = document.createElement(tag);
+            let node = document.createTextNode(text);
+            if (attributeTag) {
+                element.setAttribute(attributeTag, attributeInfo)
+            }
+            element.appendChild(node);
+            document.querySelector(parent).appendChild(element)
         }
-        element.appendChild(node);
-        document.querySelector(parent).appendChild(element)
-    }
-
-
+    
+    
         createELement("div", "#freddie", "", "id", "city");
-    // Weather
-    let usableData, fiveDayForecast, cityElement;
-    
-    
-    async function getData(){
-        try {
-            const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=814e605f58e6aaf7e798e6161f0e73fb`);
-            const jsonData = await result.json();
-            // console.log(jsonData);
-            return jsonData
-        } catch (error) {
-            // console.log(error);
+        // createELement("div", "#freddie", "Todays Forecast")
+        createELement("div", "#freddie", "", "id", "forecast");
+        createELement("div", "#freddie", "", "id", "min-temp");
+        createELement("div", "#freddie", "", "id", "max-temp");
+        // Weather
+        let usableData, fiveDayForecast, cityElement, forecastElement, minElement, maxElement;
+        
+        
+        async function getData(){
+            try {
+                const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=814e605f58e6aaf7e798e6161f0e73fb`);
+                const jsonData = await result.json();
+                // console.log(jsonData);
+                return jsonData
+            } catch (error) {
+                // console.log(error);
+            }
         }
+        
+        cityElement= document.getElementById("city");
+        forecastElement= document.getElementById("forecast");
+        minElement= document.getElementById("min-temp");
+        maxElement= document.getElementById("max-temp");
+        
+        getData().then(data => {
+            usableData = data;
+            // console.log(usableData)
+            return data;
+        }).then(data2 => {
+            console.log(data2);
+            cityElement.innerText = usableData.name;
+            forecastElement.innerText = `Todays Forecast: ${usableData.weather[0].main}`;
+            minElement.innerText = `${usableData.main.temp_min - 273.15 }C`;
+            maxElement.innerText = `${usableData.main.temp_max - 273.15 }C`
+        })
+
     }
-    
-    cityElement= document.getElementById("city");
-    
-    getData().then(data => {
-        usableData = data;
-        // console.log(usableData)
-        return data;
-    }).then(data2 => {
-        console.log(data2);
-        cityElement.innerText = usableData.name;
-    })
-
+    createWeatherBlock();
 }
+/*
+TODO
 
+* add an event listener that listens for changes in the location box and makes an api call then updates the relevant DOM elements
+* be able to save the location persistence (chrome extension)
+
+
+*/
