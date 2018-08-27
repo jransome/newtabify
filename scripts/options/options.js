aee4fb151b7a951a258ff406e34a1a2a594428b0
Sanjay Purswani <sanjsanj@hotmail.com>
2018-08-27 16:13:38 +0100

    can set stored values upon loading options

--- scripts/options/options.js
+++ scripts/options/options.js
@@ -1,29 +1,29 @@
-function readStorage(key) {
+function consoleLogStorage(key) {
   chrome.storage.sync.get(key, state => console.log(state));
 }
 
 function setStorage(object) {
   chrome.storage.sync.set(object);
-  readStorage(Object.keys(object)[0]);
+  consoleLogStorage(Object.keys(object)[0]);
 }
 
 document.querySelector("form").addEventListener("change", event => {
   switch (event.target.name) {
-    case "background-color":
+    case "backgroundColor":
       setStorage({ background: "color" });
-      document.getElementsByName("background-theme")[0].checked = false;
+      document.getElementsByName("backgroundTheme")[0].checked = false;
       break;
 
-    case "background-theme":
+    case "backgroundTheme":
       setStorage({ background: "theme" });
-      document.getElementsByName("background-color")[0].checked = false;
+      document.getElementsByName("backgroundColor")[0].checked = false;
       break;
 
-    case "input-color":
+    case "inputColor":
       setStorage({ backgroundColor: event.target.value });
       break;
 
-    case "input-theme":
+    case "inputTheme":
       setStorage({ backgroundTheme: event.target.value });
       break;
 
@@ -31,3 +31,46 @@ document.querySelector("form").addEventListener("change", event => {
       break;
   }
 });
+
+function setOption(params) {
+}
+
+function loadOptionsFromStorage() {
+  const optionKeys = ["background", "backgroundColor", "backgroundTheme"]
+
+  optionKeys.forEach(key => {
+    try {
+      // console.log(key);
+      chrome.storage.sync.get(key, state => {
+        // console.log(state[key]);
+        switch (key) {
+          case "background":
+            if (state[key] === "color") {
+              document.getElementsByName("backgroundTheme")[0].checked = false;
+              document.getElementsByName("backgroundColor")[0].checked = true;
+            } else {
+              document.getElementsByName("backgroundTheme")[0].checked = true;
+              document.getElementsByName("backgroundColor")[0].checked = false;
+            }
+            break;
+            
+          case "backgroundColor":
+            document.getElementsByName("inputColor")[0].value = state[key]
+            break;
+            
+          case "backgroundTheme":
+            document.getElementsByName("inputTheme")[0].value = state[key]
+            break;
+        
+          default:
+            break;
+        }
+        
+      });
+    } catch {
+
+    }
+  })
+}
+
+loadOptionsFromStorage()
